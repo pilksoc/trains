8c76261c0c055962fe9a7b9184883b10a406484f
Leo Durrant <poweredbykurisu@hotmail.com>
2024-07-22 00:05:31 +0100

    feat: added PoC code

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import dfpwm from 'dfpwm';
+import fs from "fs"
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+    const clips = ['next_train_plat4.wav', 'swt.wav', 'warrington.wav'];
+    const buffers = clips.map((file) => fs.readFileSync(`./media/${file}`))
+    const bufferLengths = buffers.map(buffer => buffer.length).reduce((total, length) => total + length);
+    const encoder = new dfpwm.Encoder();
+    const encodedData = encoder.encode(Buffer.concat(buffers, bufferLengths));
+
+    res.send(encodedData);
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+    return console.log(`Express is listening at http://localhost:${port}`);
 });
