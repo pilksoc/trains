803d7774275bd3310d6d9483b2934fb8804f19ef
Leo Durrant <poweredbykurisu@hotmail.com>
2024-07-23 00:56:42 +0100

    feat: leo vox

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,15 +6,28 @@ const app = express();
 const port = 3000;
 
 app.get('/', async (req, res) => {
-    const clips = ['next_train_plat4.wav', 'swt.wav', 'warrington.wav'];
-    const buffers = clips.map((file) => fs.readFileSync(`./media/${file}`))
-    const bufferLengths = buffers.map(buffer => buffer.length).reduce((total, length) => total + length);
-    const encoder = new dfpwm.Encoder();
-    const encodedData = encoder.encode(Buffer.concat(buffers, bufferLengths));
 
-    res.send(encodedData);
+    if (req.query['m'] == null) {
+        res.send('Please specify your message as a parameter "m".');
+        return;
+    }
+
+    try {
+        const msg = req.query['m'].toString().split(" ")
+        const clips = msg.map(part => `${part.toLowerCase()}.wav`);
+        const buffers = clips.map((file) => fs.readFileSync(`./media/${file}`))
+        const bufferLengths = buffers.map(buffer => buffer.length).reduce((total, length) => total + length);
+
+        if (req.query['wav'] != null) {
+            res.send(Buffer.concat(buffers, bufferLengths))
+        } else {
+            res.send(encodedData);
+        }
+    } catch (e) {
+        res.send(e);
+    }
 });
 
 app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
+    return console.log(`Leo VOX is listening at http://localhost:${port}`);
 });
