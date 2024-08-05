62be9c9aa8ae9b3a5209fb88aba376b981d9ada4
Leo Durrant <poweredbykurisu@hotmail.com>
2024-08-05 22:34:00 +0100

    feat: added a bundler (rollup)

---
+++ server/rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+    input: "src/app.ts",
+    output: {
+        file: "dist/app.js",
+        format: "cjs",
+        sourcemap: true,
+    },
+    plugins: [
+        typescript(), // translate typescript
+        json(), // import json files
+        resolve(), // tells Rollup how to find usages in node_modules
+        commonjs(), // converts to ES modules
+        production && terser(), // minify, but only in production
+    ],
+};
